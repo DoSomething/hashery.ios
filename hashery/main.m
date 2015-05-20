101f324121e05322fddf9dde154a0965c72e7c97
Tong Xiang <tong.xiang2@gmail.com>
2015-05-20 17:48:57 -0400

    Initial Commit

---
+++ hashery/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  hashery
+//
+//  Created by Tong Xiang on 5/20/15.
+//  Copyright (c) 2015 DoSomething.org. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
